ec7aec52c8d7d2f6fc5d285c5433bd43233c3c7d
Je Rome <103312494+hashira09p@users.noreply.github.com>
2024-08-16 05:03:38 +0000

    Done Assignment

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        
+        char H = 'H';
+        String numberString = "311";
+        byte zero = 0;
+        byte one = 1;
+        float number2 = 2.0f;
+        boolean isTrue = true;
+        String output = H + numberString + zero + " " + 'w' + zero + 'r' + one + 'd' + " " + number2 + " " + isTrue;
+        System.out.println(output);
+    }
+}
